fedc528c695d6fbb17ff40b512da20be189b76ee
Patrick Grady <hmfpa392@ameritech.net>
2019-02-19 22:59:17 -0500

    Code shortened

--- EasyController1_1/EasyController1_1.ino
+++ EasyController1_1/EasyController1_1.ino
@@ -17,8 +17,7 @@
 
 #define HALL_OVERSAMPLE 4
 
-//uint8_t hallToMotor[8] = {255, 255, 255, 255, 255, 255, 255, 255};
-uint8_t hallToMotor[8] = {255, 3, 5, 4, 1, 2, 0, 255}; 
+uint8_t hallToMotor[8] = {255, 255, 255, 255, 255, 255, 255, 255};
 
 void setup() {
   Serial.begin(115200);
@@ -33,18 +32,13 @@ void setup() {
   pinMode(CH_PIN, OUTPUT);
   pinMode(CL_PIN, OUTPUT);
 
-  analogWriteFrequency(AH_PIN, 8000);
-  analogWriteFrequency(BH_PIN, 8000);
-  analogWriteFrequency(CH_PIN, 8000);
-  analogWriteResolution(8);
+  analogWriteFrequency(AH_PIN, 8000);//since all pins are on the same timer, this sets PWM freq for all
 
   pinMode(HALL_1_PIN, INPUT); //set the hall pins as input
   pinMode(HALL_2_PIN, INPUT);
   pinMode(HALL_3_PIN, INPUT);
 
   pinMode(THROTTLE_PIN, INPUT);
-
-  delay(1000);
   
   identifyHalls();
 }
@@ -56,21 +50,7 @@ void loop() {
     uint8_t hall = getHalls();
     uint8_t motorState = hallToMotor[hall];
     writePWM(motorState, throttle);
-    delayMicroseconds(10);
   }
-  /*static uint8_t i = 0;
-  Serial.println(i);
-  writePWM(i++, 20);
-  delay(100);
-  i = i % 6;*/
-  /*identifyHalls();
-  delay(3000);*/
-  /*writePWM(0, 20);
-  delay(100);
-  //writePWM(4, 20);
-  //delay(100);
-  writePWM(5, 20);
-  delay(100);*/
 }
 
 void identifyHalls()
@@ -80,11 +60,11 @@ void identifyHalls()
     uint8_t prevState = (i + 1) % 6;
     Serial.print("Going to ");
     Serial.println(i);
-    for(uint8_t j = 0; j < 50; j++)
+    for(uint16_t j = 0; j < 200; j++)
     {
-      delay(10);
+      delay(1);
       writePWM(i, 20);
-      delay(10);
+      delay(1);
       writePWM(prevState, 20);
     }
     hallToMotor[getHalls()] = (i + 2) % 6;
@@ -98,8 +78,6 @@ void identifyHalls()
     Serial.print(", ");
   }
   Serial.println();
-
-  //while(1);
 }
 
 void writePWM(uint8_t motorState, uint8_t dutyCycle)
@@ -107,58 +85,20 @@ void writePWM(uint8_t motorState, uint8_t dutyCycle)
   if(dutyCycle == 0)
     motorState = 255;//if zero throttle, turn all switches off
 
-  switch(motorState){
-    case 0://LOW A, HIGH B
-      analogWrite(AH_PIN, 0);
-      analogWrite(CH_PIN, 0);
-      analogWrite(BH_PIN, dutyCycle);
-      digitalWrite(BL_PIN, 0);
-      digitalWrite(CL_PIN, 0);
-      digitalWrite(AL_PIN, 1);
-      break;
-    case 1://LOW A, HIGH C
-      analogWrite(AH_PIN, 0);
-      analogWrite(BH_PIN, 0);
-      analogWrite(CH_PIN, dutyCycle);
-      digitalWrite(BL_PIN, 0);
-      digitalWrite(CL_PIN, 0);
-      digitalWrite(AL_PIN, 1);
-      break;
-    case 2://LOW B, HIGH C
-      analogWrite(AH_PIN, 0);
-      analogWrite(BH_PIN, 0);
-      analogWrite(CH_PIN, dutyCycle);
-      digitalWriteFast(AL_PIN, 0);
-      digitalWriteFast(CL_PIN, 0);
-      digitalWriteFast(BL_PIN, 1);
-      break;
-    case 3://LOW B, HIGH A
-      analogWrite(BH_PIN, 0);
-      analogWrite(CH_PIN, 0);
-      analogWrite(AH_PIN, dutyCycle);
-      digitalWriteFast(AL_PIN, 0);
-      digitalWriteFast(CL_PIN, 0);
-      digitalWriteFast(BL_PIN, 1);
-      break;
-    case 4://LOW C, HIGH A
-      analogWrite(BH_PIN, 0);
-      analogWrite(CH_PIN, 0);
-      analogWrite(AH_PIN, dutyCycle);
-      digitalWriteFast(AL_PIN, 0);
-      digitalWriteFast(BL_PIN, 0);
-      digitalWriteFast(CL_PIN, 1);
-      break;
-    case 5://LOW C, HIGH B
-      analogWrite(AH_PIN, 0);
-      analogWrite(CH_PIN, 0);
-      analogWrite(BH_PIN, dutyCycle);
-      digitalWriteFast(AL_PIN, 0);
-      digitalWriteFast(BL_PIN, 0);
-      digitalWriteFast(CL_PIN, 1);
-      break;
-    default://all off
+  if(motorState == 0)//LOW A, HIGH B
+      writePhases(0, dutyCycle, 0, 1, 0, 0);
+  else if(motorState == 1)//LOW A, HIGH C
+      writePhases(0, 0, dutyCycle, 1, 0, 0);
+  else if(motorState == 2)//LOW B, HIGH C
+      writePhases(0, 0, dutyCycle, 0, 1, 0);
+  else if(motorState == 3)//LOW B, HIGH A
+      writePhases(dutyCycle, 0, 0, 0, 1, 0);
+  else if(motorState == 4)//LOW C, HIGH A
+      writePhases(dutyCycle, 0, 0, 0, 0, 1);
+  else if(motorState == 5)//LOW C, HIGH B
+      writePhases(0, dutyCycle, 0, 0, 0, 1);
+  else//all off
       writePhases(0, 0, 0, 0, 0, 0);
-  }
 }
 
 void writePhases(uint8_t ah, uint8_t bh, uint8_t ch, uint8_t al, uint8_t bl, uint8_t cl)
@@ -207,4 +147,3 @@ uint8_t readThrottle()
   
   return adc;
 }
-
